792136919b222cd2603c21d1d8ebcda50704f082
TheComamba <thecomamba@jojoheinze.de>
2023-05-21 18:09:03 +0200

    Deleting old changelog.

--- changelogs/get_current.sh
+++ changelogs/get_current.sh
@@ -3,5 +3,5 @@
 set -e
 
 latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
-git_workspace=$(git rev-parse --show-toplevel)
-cp $git_workspace/changelogs/$latest_tag.md $git_workspace/changelogs/current.md
+filepath=$(dirname $(find . -name $latest_tag.md))
+cp $filepath/$latest_tag.md $filepath/current.md
