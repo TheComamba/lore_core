094b6573b72277d4c1eb62f87875c1c9de842549
TheComamba <thecomamba@jojoheinze.de>
2023-05-24 21:53:37 +0200

    Updating workflow.

--- install_build_dependencies_macos.sh
+++ install_build_dependencies_macos.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e
 
-rust_rfd_req=libgtk-3-dev
 rust_rusqlite_req=libsqlite3-dev
 
 brew update
-brew install $rust_rfd_req $rust_rusqlite_req
+brew install $rust_rusqlite_req
 
 if ! command -v cargo &> /dev/null
 then
