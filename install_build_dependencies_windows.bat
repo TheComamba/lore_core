1f2ba682bec15ceffa8e848cbb21537b37bf51a4
TheComamba <thecomamba@jojoheinze.de>
2023-05-25 21:14:16 +0200

    Updating workflow.

--- install_build_dependencies_windows.bat
+++ install_build_dependencies_windows.bat
@@ -1,2 +1,8 @@
 
+if not exist %SystemRoot%\System32\choco.exe (
+    @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+)
+
 choco install sqlite -y
+
+setx PATH "%PATH%;C:\ProgramData\chocolatey\lib\SQLite\tools" /M
