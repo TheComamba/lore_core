dcb076f18ff8cbd34cdb39c2c4851a13f3390990
TheComamba <thecomamba@jojoheinze.de>
2023-05-28 15:14:34 +0200

    -y option for install script

--- scripts/install_build_dependencies_linux.sh
+++ scripts/install_build_dependencies_linux.sh
@@ -4,11 +4,11 @@ set -e
 rust_rfd_req=libgtk-3-dev
 
 sudo apt-get update
-sudo apt-get install $rust_rfd_req
+sudo apt-get install -y $rust_rfd_req
 
 if ! command -v cargo &> /dev/null
 then
     echo Installing rust...
-    sudo apt-get install curl
+    sudo apt-get install -y curl
     sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
--- scripts/install_runtime_dependencies_linux.sh
+++ scripts/install_runtime_dependencies_linux.sh
@@ -2,4 +2,4 @@
 set -e
 
 sudo apt-get update
-sudo apt-get install libgtk-3-dev
+sudo apt-get install -y libgtk-3-dev
