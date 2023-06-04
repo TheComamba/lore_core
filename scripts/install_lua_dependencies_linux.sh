93f463d3913deae984498de16122e7094d7e043b
TheComamba <thecomamba@jojoheinze.de>
2023-06-04 13:53:54 +0200

    Also installing liblua-dev.

--- scripts/install_lua_dependencies_linux.sh
+++ scripts/install_lua_dependencies_linux.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
+version=5.2
 sudo apt-get update
-sudo apt-get install -y lua5.2 luarocks
+sudo apt-get install -y lua$version liblua$version-dev luarocks
 
 # luainc=$(pkg-config --cflags lua)
-export LUA_INCDIR=/usr/lib/x86_64-linux-gnu/lua/
+# export LUA_INCDIR=/usr/lib/x86_64-linux-gnu/lua/
 
 luarocks install --local --server=https://luarocks.org/dev luaffi
 
