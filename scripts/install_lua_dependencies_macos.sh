7842532993a745fc0e660900a495158b25b76232
TheComamba <thecomamba@jojoheinze.de>
2023-06-04 14:16:31 +0200

    Searching for testfile path.

--- scripts/install_lua_dependencies_macos.sh
+++ scripts/install_lua_dependencies_macos.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-version=5.1
+version=5.2
 brew update
 brew install lua@$version luarocks
 
 luarocks install --lua-dir=/usr/local/opt/lua@5.1 --server=https://luarocks.org/dev luaffi
 
 echo Checking installation...
-lua ffitest.lua
+find . -name ffitest.lua -exec lua {} \;
--- scripts/install_lua_dependencies_windows.bat
+++ scripts/install_lua_dependencies_windows.bat
@@ -1,7 +1,9 @@
+@echo off
+
 choco install -y lua
 choco install -y luarocks
 
 luarocks install --server=https://luarocks.org/dev luaffi
 
 echo Checking installation...
-lua ffitest.lua
+for /f usebackq %F in (`where /r . ffitest.lua`) do lua %F
