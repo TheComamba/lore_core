fa4c31200df6b0ef86c3aa2983302168630ffd6e
TheComamba <thecomamba@jojoheinze.de>
2023-06-04 13:20:50 +0200

    Listing files takes too long.

--- scripts/install_lua_dependencies_linux.sh
+++ scripts/install_lua_dependencies_linux.sh
@@ -7,9 +7,6 @@ sudo apt-get install -y lua5.2 luarocks
 # luainc=$(pkg-config --cflags lua)
 export LUA_INCDIR=/usr/lib/:/usr/local/lib/:$luainc
 
-ls -R /usr/lib/
-ls -R /usr/local/lib/
-
 luarocks install --local --server=https://luarocks.org/dev luaffi
 
 echo Checking installation...
