a6b2df71baa714c9f23fdc348fcd606cf86dafb1
TheComamba <thecomamba@jojoheinze.de>
2023-06-04 13:41:09 +0200

    Clearer print.

--- scripts/install_lua_dependencies_linux.sh
+++ scripts/install_lua_dependencies_linux.sh
@@ -7,9 +7,10 @@ sudo apt-get install -y lua5.2 luarocks
 # luainc=$(pkg-config --cflags lua)
 export LUA_INCDIR=/usr/lib/:/usr/local/lib/:/usr/lib/x86_64-linux-gnu/lua/:$luainc
 
+echo HERE!!!!
 echo $LUA_INCDIR
 
-luarocks install --local --server=https://luarocks.org/dev luaffi
+# luarocks install --local --server=https://luarocks.org/dev luaffi
 
-echo Checking installation...
-lua ffitest.lua
+# echo Checking installation...
+# lua ffitest.lua
