6c321f1ded77cbf50e37617dd8874294ed4d2e30
TheComamba <thecomamba@jojoheinze.de>
2023-06-04 10:48:16 +0200

    Trying hardcoded paths.

--- scripts/install_lua_dependencies_linux.sh
+++ scripts/install_lua_dependencies_linux.sh
@@ -4,8 +4,11 @@ set -e
 sudo apt-get update
 sudo apt-get install -y lua5.2 luarocks
 
-luainc=$(pkg-config --cflags lua)
-export LUA_INCDIR=$LUA_INCDIR:$luainc
+# luainc=$(pkg-config --cflags lua)
+export LUA_INCDIR=/usr/lib/:/usr/local/lib/:$luainc
+
+ls -R /usr/lib/Lua*
+ls -R /usr/local/lib/Lua*
 
 luarocks install --local --server=https://luarocks.org/dev luaffi
 
