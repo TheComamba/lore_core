301a73338519461368bc19781ced867021febef3
TheComamba <thecomamba@jojoheinze.de>
2023-06-04 16:28:58 +0200

    Removing temporary test_lua workflow again.

--- scripts/install_lua_dependencies_windows.bat
+++ scripts/install_lua_dependencies_windows.bat
@@ -10,4 +10,5 @@ luarocks install --server=https://luarocks.org/dev luaffi
 refreshenv
 
 echo Checking installation...
+rem Yep. That's how you pass output of one command to another in Windows.
 for /f usebackq %F in (`where /r . ffitest.lua`) do lua %F
