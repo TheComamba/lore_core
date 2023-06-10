fe98f78f79bd2dc2d9ec9b30e3be20152eea1bba
TheComamba <thecomamba@jojoheinze.de>
2023-06-10 13:26:11 +0200

    Using rust action instead of manually installing it.

--- scripts/install_build_dependencies_linux.sh
+++ scripts/install_build_dependencies_linux.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 set -e
 
-rust_req=(build-essential cmake pkg-config libfontconfig1-dev)
 rust_rfd_req=libgtk-3-dev
 
 sudo apt-get update
-sudo apt-get install -y "${rust_req[@]}" "$rust_rfd_req"
-
-if ! command -v cargo &> /dev/null
-then
-    echo Installing rust...
-    sudo apt-get install -y curl
-    sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-fi
+sudo apt-get install -y "$rust_rfd_req"
--- scripts/install_build_dependencies_windows.bat
+++
@@ -1,4 +0,0 @@
-
-if not exist %SystemRoot%\System32\choco.exe (
-    @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-)
