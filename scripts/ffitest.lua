51f9a24f76a1de6cd42741e929f0ba141af80dbc
TheComamba <thecomamba@jojoheinze.de>
2023-06-10 09:11:56 +0200

    Replacing lua with python in workflow.

--- scripts/ffitest.lua
+++
@@ -1,4 +0,0 @@
-local ffi = require("ffi")
-ffi.cdef [[void Sleep(int ms); int poll(struct pollfd *fds, unsigned long nfds, int timeout);]]
-print("Things are looking good.")
-return function(s) ffi.C.poll(nil, 0, s * 1000) end
