927dcb72857ba8a6cca673c4885454e631b85e8c
TheComamba <thecomamba@jojoheinze.de>
2023-06-11 10:33:32 +0200

    Correction.

--- changelogs/get_current.sh
+++ changelogs/get_current.sh
@@ -5,8 +5,9 @@ cd $(git rev-parse --show-toplevel)
 dir=changelogs
 latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
 filepath=$dir/$latest_tag.md
-if [ -z "$filepath" ]; then
-    echo "[No changelog found]" > $dir/current.md
-else
+if test -f "$filepath"; then
     cp $filepath $dir/current.md
+else
+    echo "No changelog found for tag $latest_tag."
+    echo "[No changelog found]" > $dir/current.md
 fi
