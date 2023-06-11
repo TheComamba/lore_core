380ae9bb71ce232d04b15067c22828ea7af2e329
TheComamba <thecomamba@jojoheinze.de>
2023-06-11 10:25:03 +0200

    Something like error handling for changelog.

--- changelogs/get_current.sh
+++ changelogs/get_current.sh
@@ -3,5 +3,10 @@
 set -e
 
 latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
-filepath=$(dirname $(find . -name $latest_tag.md))
-cp $filepath/$latest_tag.md $filepath/current.md
+dir=$(dirname $(find . -name $latest_tag.md))
+filepath=$dir/$latest_tag.md
+if [ -z "$filepath" ]; then
+    echo "[No changelog found]" > $dir/current.md
+else
+    cp $filepath $dir/current.md
+fi
