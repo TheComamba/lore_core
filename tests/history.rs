131de318dc110f29cec8b806c6a90880a0ae8cfa
TheComamba <thecomamba@jojoheinze.de>
2023-08-27 21:47:45 +0200

    Tests for writing many rows to database.

--- tests/history.rs
+++ tests/history.rs
@@ -23,3 +23,50 @@ fn write_single_history_item() {
     assert!(item == item_out[0]);
     temp_path.close().unwrap();
 }
+
+#[test]
+fn write_many_history_items() {
+    let temp_path = NamedTempFile::new().unwrap().into_temp_path();
+    let path_in: PathBuf = temp_path.as_os_str().into();
+    let db = LoreDatabase::open(path_in.clone()).unwrap();
+
+    let contents = vec!["testcontent1".to_string(), "testcontent2".to_string()];
+    let days = vec![Some(1), None];
+    let is_concerns_others = vec![true, false];
+    let is_secret = vec![true, false];
+    let originators = vec![Some("testoriginator1".to_string()), None];
+    let mut items: Vec<HistoryItem> = Vec::new();
+    for content in contents.iter() {
+        for concerns_others in is_concerns_others.iter() {
+            for secret in is_secret.iter() {
+                for originator in originators.iter() {
+                    for day in days.iter() {
+                        let unique_label = day.map(|d| d.to_string()).unwrap_or("".to_string())
+                            + content
+                            + &concerns_others.to_string()
+                            + &secret.to_string()
+                            + &originator.clone().map(|o| o).unwrap_or("".to_string());
+                        items.push(HistoryItem {
+                            year: 2020,
+                            day: day.clone(),
+                            label: unique_label,
+                            content: content.clone(),
+                            is_concerns_others: concerns_others.clone(),
+                            is_secret: secret.clone(),
+                            originator: originator.clone(),
+                        });
+                    }
+                }
+            }
+        }
+    }
+
+    db.write_history_items(items.clone()).unwrap();
+
+    let items_out = db.get_all_history_items().unwrap();
+    assert!(items.len() == items_out.len());
+    for item in items.iter() {
+        assert!(items_out.contains(item));
+    }
+    temp_path.close().unwrap();
+}
--- tests/relationships.rs
+++ tests/relationships.rs
@@ -18,3 +18,63 @@ fn write_single_relationship() {
     assert!(rel == rel_out[0]);
     temp_path.close().unwrap();
 }
+
+#[test]
+fn write_many_relationships() {
+    let temp_path = NamedTempFile::new().unwrap().into_temp_path();
+    let path_in: PathBuf = temp_path.as_os_str().into();
+    let db = LoreDatabase::open(path_in.clone()).unwrap();
+
+    let parents = vec!["testparent1".to_string(), "testparent2".to_string()];
+    let children = vec!["testchild1".to_string(), "testchild2".to_string()];
+    let roles = vec![Some("testrole".to_string()), None];
+    let mut rels: Vec<EntityRelationship> = Vec::new();
+    for parent in parents.iter() {
+        for child in children.iter() {
+            for role in roles.iter() {
+                rels.push(EntityRelationship {
+                    parent: parent.clone(),
+                    child: child.clone(),
+                    role: role.clone(),
+                });
+            }
+        }
+    }
+
+    db.write_relationships(rels.clone()).unwrap();
+
+    let rels_out = db.get_relationships().unwrap();
+    assert!(rels.len() == rels_out.len());
+    for rel in rels.iter() {
+        assert!(rels_out.contains(rel));
+    }
+    temp_path.close().unwrap();
+}
+
+#[test]
+fn writing_several_roles_to_same_relationship() {
+    let temp_path = NamedTempFile::new().unwrap().into_temp_path();
+    let path_in: PathBuf = temp_path.as_os_str().into();
+    let db = LoreDatabase::open(path_in.clone()).unwrap();
+
+    let parent = "testparent".to_string();
+    let child = "testchild".to_string();
+    let roles = vec!["testrole1".to_string(), "testrole2".to_string()];
+    let mut rels: Vec<EntityRelationship> = Vec::new();
+    for role in roles.iter() {
+        rels.push(EntityRelationship {
+            parent: parent.clone(),
+            child: child.clone(),
+            role: Some(role.clone()),
+        });
+    }
+
+    db.write_relationships(rels.clone()).unwrap();
+
+    let rels_out = db.get_relationships().unwrap();
+    assert!(rels.len() == rels_out.len());
+    for rel in rels.iter() {
+        assert!(rels_out.contains(rel));
+    }
+    temp_path.close().unwrap();
+}
