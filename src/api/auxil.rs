2ed9c13977947be48cc13fc4f90e97d2f66d9912
TheComamba <thecomamba@jojoheinze.de>
2023-09-03 21:47:39 +0200

    Tests for char_ptr conversions.

--- src/api/auxil.rs
+++ src/api/auxil.rs
@@ -37,3 +37,79 @@ pub(super) fn optional_string_to_char_pointer(string: &Option<String>) -> *const
 pub(super) fn char_ptr(message: &str) -> *const libc::c_char {
     CString::new(message).unwrap().into_raw()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::ptr;
+
+    #[test]
+    fn test_char_pointer_to_string() {
+        let string = "Eyjafjallajökull!";
+        let char_pointer = string_to_char_pointer(string);
+        let result = char_pointer_to_string(char_pointer);
+        assert_eq!(result.unwrap(), string);
+    }
+
+    #[test]
+    fn test_null_pointer_to_string() {
+        let char_pointer = ptr::null();
+        let result = char_pointer_to_string(char_pointer);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_empty_string_to_char_pointer() {
+        let string = "";
+        let char_pointer = string_to_char_pointer(string);
+        let result = char_pointer_to_string(char_pointer);
+        assert_eq!(result.unwrap(), string);
+    }
+
+    #[test]
+    fn test_char_pointer_to_optional_string() {
+        let string = "Eyjafjallajökull!";
+        let char_pointer = string_to_char_pointer(string);
+        let result = char_pointer_to_optional_string(char_pointer);
+        assert_eq!(result.unwrap(), Some(string.to_string()));
+    }
+
+    #[test]
+    fn test_null_pointer_to_optional_string() {
+        let char_pointer = ptr::null();
+        let result = char_pointer_to_optional_string(char_pointer);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_string_to_char_pointer() {
+        let string = "Eyjafjallajökull!";
+        let char_pointer = string_to_char_pointer(string);
+        let result = char_pointer_to_string(char_pointer);
+        assert_eq!(result.unwrap(), string);
+    }
+
+    #[test]
+    fn test_optional_string_to_char_pointer() {
+        let string_opt = Some("Eyjafjallajökull!".to_string());
+        let char_pointer = optional_string_to_char_pointer(&string_opt);
+        let result = char_pointer_to_optional_string(char_pointer);
+        assert_eq!(result.unwrap(), string_opt);
+    }
+
+    #[test]
+    fn test_empty_optional_string_to_char_pointer() {
+        let string_opt = Some("".to_string());
+        let char_pointer = optional_string_to_char_pointer(&string_opt);
+        let result = char_pointer_to_optional_string(char_pointer);
+        assert_eq!(result.unwrap(), None);
+    }
+
+    #[test]
+    fn test_none_optional_string_to_char_pointer() {
+        let string_opt = None;
+        let char_pointer = optional_string_to_char_pointer(&string_opt);
+        let result = char_pointer_to_optional_string(char_pointer);
+        assert_eq!(result.unwrap(), None);
+    }
+}
