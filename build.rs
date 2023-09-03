0e0bca627adb381e33fc99ff5b9c3aa81fff51d0
TheComamba <thecomamba@jojoheinze.de>
2023-09-03 16:25:21 +0200

    Making schema generation part of cargo build. This makes diesel_cli mandatory.

--- build.rs
+++ build.rs
@@ -1,10 +1,11 @@
 extern crate cbindgen;
 
-use std::env;
+use std::{env, process::Command};
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
+    // Generate C header file
     cbindgen::Builder::new()
         .with_crate(crate_dir)
         .with_no_includes()
@@ -12,4 +13,10 @@ fn main() {
         .generate()
         .expect("Unable to generate C header file.")
         .write_to_file("lorecore_api.h");
+
+    // Create/Update schema.rs
+    let mut cmd = Command::new("diesel");
+    cmd.arg("print-schema").arg("--database-url=example.db");
+    let schema = cmd.output().expect("Failed to execute diesel migrations");
+    std::fs::write("src/sql/schema.rs", schema.stdout).expect("Failed to write schema.rs");
 }
--- scripts/install_build_dependencies_linux.sh
+++ scripts/install_build_dependencies_linux.sh
@@ -5,3 +5,5 @@ rust_rfd_req=libgtk-3-dev
 
 sudo apt-get update
 sudo apt-get install -y "$rust_rfd_req"
+
+cargo install diesel_cli --no-default-features --features sqlite
