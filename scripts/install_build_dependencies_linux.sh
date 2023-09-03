dc4bb731cd92edb226fb6f4ecaafb2120bde7b45
TheComamba <thecomamba@jojoheinze.de>
2023-09-03 17:18:05 +0200

    Installing libsqlite3-dev.

--- scripts/install_build_dependencies_linux.sh
+++ scripts/install_build_dependencies_linux.sh
@@ -2,9 +2,10 @@
 set -e
 
 rust_rfd_req=libgtk-3-dev
-diesel_req=sqlite3
+diesel_req1=sqlite3
+diesel_req2=libsqlite3-dev
 
 sudo apt-get update
-sudo apt-get install -y "$rust_rfd_req" "$diesel_req"
+sudo apt-get install -y $rust_rfd_req $diesel_req1 $diesel_req2
 
 cargo --locked install diesel_cli --no-default-features --features sqlite
