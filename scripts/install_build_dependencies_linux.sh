d9dc63dd12f3a5c332670f9ff01d89c20a2b9d65
TheComamba <thecomamba@jojoheinze.de>
2023-09-03 17:04:39 +0200

    Installing diesel with lockfile on linux.

--- scripts/install_build_dependencies_linux.sh
+++ scripts/install_build_dependencies_linux.sh
@@ -7,4 +7,4 @@ diesel_req=sqlite3
 sudo apt-get update
 sudo apt-get install -y "$rust_rfd_req" "$diesel_req"
 
-cargo install diesel_cli --no-default-features --features sqlite
+cargo --locked install diesel_cli --no-default-features --features sqlite
