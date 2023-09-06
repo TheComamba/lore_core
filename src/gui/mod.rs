1e6ddb366b6b75ab98ced8e4a1ec8f19f4793da4
TheComamba <thecomamba@jojoheinze.de>
2023-09-06 21:09:44 +0200

    Removed LoreGui.

--- src/gui/mod.rs
+++
@@ -1,9 +0,0 @@
-pub(super) mod app;
-pub(super) mod db_col_view;
-mod dialog;
-mod entity_view;
-mod file_dialogs;
-mod history_view;
-mod relationship_view;
-mod style;
-mod user_preferences;
--- src/gui/style.rs
+++
@@ -1,5 +0,0 @@
-use iced::widget::{text, Text};
-
-pub(super) fn header(content: &str) -> Text {
-    text(content).size(25)
-}
