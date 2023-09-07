21d55f5866e6275f30e9e417b29b9e447e541eb6
TheComamba <thecomamba@jojoheinze.de>
2023-09-08 00:00:03 +0200

    Adjusting dependency script.

--- scripts/install_build_dependencies_linux.sh
+++ scripts/install_build_dependencies_linux.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -e
 
-rust_rfd_req=libgtk-3-dev
-
-sudo apt-get update
-sudo apt-get install -y $rust_rfd_req
-
 cargo --locked install diesel_cli --no-default-features --features sqlite-bundled
