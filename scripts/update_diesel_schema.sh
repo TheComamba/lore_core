b96b2fe5e88d24ec340f71b0ad86d98699a7be76
TheComamba <thecomamba@jojoheinze.de>
2023-12-24 14:17:14 +0100

    Refining diesel script.

--- scripts/update_diesel_schema.sh
+++ scripts/update_diesel_schema.sh
@@ -4,10 +4,11 @@ set -e
 # Go to the root directory of the Git repository
 cd "$(git rev-parse --show-toplevel)"
 
-if ! command -v diesel_cli &> /dev/null; then
+if ! command -v diesel &> /dev/null; then
     cargo --locked install diesel_cli --no-default-features --features sqlite-bundled
 fi
 
 diesel setup --database-url=dummy.db
 diesel print-schema --database-url=dummy.db > src/sql/schema.rs
 rm dummy.db
+cargo fmt
