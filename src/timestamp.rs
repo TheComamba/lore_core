6ee282c0ed8ecdad4e1e93fc6b6104c5abc9a4eb
TheComamba <thecomamba@jojoheinze.de>
2024-03-13 21:17:23 +0100

    Refactor timestamp handling to use AtomicI64

--- src/timestamp.rs
+++ src/timestamp.rs
@@ -1,19 +1,28 @@
-use std::{
-    sync::Mutex,
-    time::{SystemTime, UNIX_EPOCH},
-};
+use std::sync::atomic::{AtomicI64, Ordering};
+use std::time::{SystemTime, UNIX_EPOCH};
 
-static LAST_TIMESTAMP: Mutex<i64> = Mutex::new(0);
+static LAST_TIMESTAMP: AtomicI64 = AtomicI64::new(0);
 
 pub fn current_timestamp() -> i64 {
     let now = SystemTime::now();
-    let mut ns_since_the_epoch = now.duration_since(UNIX_EPOCH).unwrap().as_nanos() as i64;
-    let mut last_timestamp = LAST_TIMESTAMP.lock().unwrap();
-    while *last_timestamp >= ns_since_the_epoch {
-        ns_since_the_epoch += 1;
+    let mus_since_the_epoch = now.duration_since(UNIX_EPOCH).unwrap().as_micros() as i64;
+
+    let mut last_timestamp = LAST_TIMESTAMP.load(Ordering::SeqCst);
+    while mus_since_the_epoch <= last_timestamp {
+        let new_timestamp = last_timestamp + 1;
+        match LAST_TIMESTAMP.compare_exchange_weak(
+            last_timestamp,
+            new_timestamp,
+            Ordering::SeqCst,
+            Ordering::SeqCst,
+        ) {
+            Ok(_) => return new_timestamp,
+            Err(x) => last_timestamp = x,
+        }
     }
-    *last_timestamp = ns_since_the_epoch;
-    ns_since_the_epoch
+
+    LAST_TIMESTAMP.store(mus_since_the_epoch, Ordering::SeqCst);
+    mus_since_the_epoch
 }
 
 #[cfg(test)]
@@ -21,31 +30,35 @@ mod tests {
     use super::*;
 
     #[test]
-    fn timestamps_are_distinct() {
-        let mut timestamps = vec![];
-        for _ in 0..1000 {
-            timestamps.push(current_timestamp());
-        }
-        for (i, t_i) in timestamps.iter().enumerate() {
-            for (j, t_j) in timestamps.iter().enumerate() {
-                if i != j {
-                    assert!(t_i != t_j);
-                }
-            }
+    fn timestamps_are_ascending() {
+        let mut last_timestamp = current_timestamp();
+        for i in 1..1_000_000 {
+            let current_timestamp = current_timestamp();
+            assert!(
+                last_timestamp < current_timestamp,
+                "i={}, t_(i-1)={}, t_i={}",
+                i,
+                last_timestamp,
+                current_timestamp
+            );
+            last_timestamp = current_timestamp;
         }
     }
 
     #[test]
-    fn timestamps_are_ascending() {
-        let mut timestamps = vec![];
-        for _ in 0..1000 {
-            timestamps.push(current_timestamp());
+    fn test_current_timestamp_performance() {
+        use std::time::Instant;
+
+        let start = Instant::now();
+        for _ in 0..1_000_000 {
+            current_timestamp();
         }
-        for i in 1..timestamps.len() {
-            let t_im1 = timestamps[i - 1];
-            let t_i = timestamps[i];
-            println!("i={}, t_(i-1)={}, t_i={}", i, t_im1, t_i);
-            assert!(t_im1 < t_i);
-        }
+        let duration = start.elapsed();
+
+        assert!(
+            duration.as_secs_f64() < 1.0,
+            "Performance test failed. Duration: {:?}",
+            duration
+        );
     }
 }
