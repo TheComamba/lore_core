0843cce776bdff2d3663b37a688fdc094bb3421a
TheComamba <thecomamba@jojoheinze.de>
2024-05-30 14:06:45 +0200

    Removed unused functions.

--- src/c_api/auxil.rs
+++ src/c_api/auxil.rs
@@ -20,26 +20,8 @@ pub(super) unsafe fn char_pointer_to_string(
     Ok(string.to_string())
 }
 
-pub(super) unsafe fn char_pointer_to_optional_string(
-    string: *const libc::c_char,
-) -> Result<Option<String>, LoreCoreError> {
-    let string = char_pointer_to_string(string)?;
-    Ok(if string.is_empty() {
-        None
-    } else {
-        Some(string)
-    })
-}
-
 pub(super) fn string_to_char_pointer(string: &str) -> *const libc::c_char {
     CString::new(string).unwrap().into_raw()
-}
-
-pub(super) fn optional_string_to_char_pointer(string: &Option<String>) -> *const libc::c_char {
-    match string {
-        Some(string) => string_to_char_pointer(string),
-        None => string_to_char_pointer(""),
-    }
 }
 
 pub(super) fn char_ptr(message: &str) -> *const libc::c_char {
@@ -86,49 +68,10 @@ mod tests {
     }
 
     #[test]
-    fn test_char_pointer_to_optional_string() {
-        let string = "Eyjafjallajökull!";
-        let char_pointer = string_to_char_pointer(string);
-        let result = unsafe { char_pointer_to_optional_string(char_pointer) };
-        assert_eq!(result.unwrap(), Some(string.to_string()));
-    }
-
-    #[test]
-    fn test_null_pointer_to_optional_string() {
-        let char_pointer = ptr::null();
-        let result = unsafe { char_pointer_to_optional_string(char_pointer) };
-        assert!(result.is_err());
-    }
-
-    #[test]
     fn test_string_to_char_pointer() {
         let string = "Eyjafjallajökull!";
         let char_pointer = string_to_char_pointer(string);
         let result = unsafe { char_pointer_to_string(char_pointer) };
         assert_eq!(result.unwrap(), string);
-    }
-
-    #[test]
-    fn test_optional_string_to_char_pointer() {
-        let string_opt = Some("Eyjafjallajökull!".to_string());
-        let char_pointer = optional_string_to_char_pointer(&string_opt);
-        let result = unsafe { char_pointer_to_optional_string(char_pointer) };
-        assert_eq!(result.unwrap(), string_opt);
-    }
-
-    #[test]
-    fn test_empty_optional_string_to_char_pointer() {
-        let string_opt = Some("".to_string());
-        let char_pointer = optional_string_to_char_pointer(&string_opt);
-        let result = unsafe { char_pointer_to_optional_string(char_pointer) };
-        assert_eq!(result.unwrap(), None);
-    }
-
-    #[test]
-    fn test_none_optional_string_to_char_pointer() {
-        let string_opt = None;
-        let char_pointer = optional_string_to_char_pointer(&string_opt);
-        let result = unsafe { char_pointer_to_optional_string(char_pointer) };
-        assert_eq!(result.unwrap(), None);
     }
 }
