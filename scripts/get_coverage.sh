7006594cd246cb872df814b046d17ac5e28fc1b5
TheComamba <thecomamba@jojoheinze.de>
2025-08-23 18:58:14 +0200

    Installing missing binary instead of complaining.

--- scripts/get_coverage.sh
+++ scripts/get_coverage.sh
@@ -6,8 +6,8 @@ cd "$(git rev-parse --show-toplevel)"
 # Check if cargo-tarpaulin is installed
 if ! cargo tarpaulin --version &> /dev/null
 then
-    echo "cargo-tarpaulin could not be found, please install it first using 'cargo install cargo-tarpaulin'"
-    exit
+    echo "cargo-tarpaulin could not be found, installing it..."
+    cargo install cargo-tarpaulin
 fi
 
 # Run tests with coverage
